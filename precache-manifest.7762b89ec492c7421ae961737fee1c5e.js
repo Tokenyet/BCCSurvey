803495cee1bfdf4e1055350d45b1b12cab6dc553
Dowen <tokenyete@gmail.com>
2019-04-18 22:39:15 +0800

    Updates

---
+++ precache-manifest.7762b89ec492c7421ae961737fee1c5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bcb91142c2f4a06ea96db8098d8236f",
+    "url": "/BCCSurvey/static/media/pic_2_indroduction@3x.1bcb9114.png"
+  },
+  {
+    "revision": "c0a9b8535beb880d3b28c2e59f091f7e",
+    "url": "/BCCSurvey/static/media/pic_1_main page.c0a9b853.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BCCSurvey/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "89f7330ca872bd58fa2534642c212f68",
+    "url": "/BCCSurvey/static/media/icon_arrow.89f7330c.svg"
+  },
+  {
+    "revision": "cea48e1e02af7282d611",
+    "url": "/BCCSurvey/static/js/runtime~main.3b6e59a7.js"
+  },
+  {
+    "revision": "de2cfaab6a5891b33661",
+    "url": "/BCCSurvey/static/js/main.3df4c493.chunk.js"
+  },
+  {
+    "revision": "69e3f3cdaf366db86b48",
+    "url": "/BCCSurvey/static/js/2.2c34bab6.chunk.js"
+  },
+  {
+    "revision": "de2cfaab6a5891b33661",
+    "url": "/BCCSurvey/static/css/main.63897cad.chunk.css"
+  },
+  {
+    "revision": "4e7638dd264eaef2bb5fef94c81c2386",
+    "url": "/BCCSurvey/index.html"
+  }
+];
